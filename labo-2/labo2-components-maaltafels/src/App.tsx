a1cf63e6cea5e9fd49fc34b6c4f1904c52c93c5c
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-02-10 20:58:46 +0000

    labo2-components-maaltafels klaar

---
+++ labo-2/labo2-components-maaltafels/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MultiplicationTable from "./components/MultiplicationTable/MultiplicationTable";
+
+function App() {
+  return (
+    <>
+      <MultiplicationTable max={10} />
+    </>
+  );
+}
+
+export default App;
