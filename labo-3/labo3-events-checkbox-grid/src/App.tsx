f80e3eb61b415da0d2274bc0f1902e8b652928a8
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-02-11 20:11:30 +0000

    labo3 update

---
+++ labo-3/labo3-events-checkbox-grid/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CheckBox from "./components/CheckBox/CheckBox";
+
+function App() {
+  return (
+    <>
+      <CheckBox size={10} />
+    </>
+  );
+}
+
+export default App;
