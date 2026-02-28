e0667c5f0798d4a82d6a2f12819507234f924808
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-02-28 21:22:35 +0000

    labo5-state-alien-alphabet klaar

---
+++ labo-5/labo5-state-alien-alphabet/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AlienAlphabet from "./components/AlienAlphabet/AlienAlphabet";
+
+function App() {
+  return (
+    <>
+      <AlienAlphabet />
+    </>
+  );
+}
+
+export default App;
